369850c8694ab36dc95e9ac5959c07fcaa93575f
nop <noplab90@gmail.com>
2026-05-14 10:18:24 +0900

    chore(matlab): remove old paths superseded by restructure
    Finishes the move in the previous commit by deleting the original
    locations of files now living under workflows/, platforms/, and the
    artifacts/ tree (which is ignored). The derived/ database and
    build/cosim/*.mat goldens were accidentally tracked test caches and
    will be regenerated on demand.

--- matlab/board_support/hdlcoder_board_customization.m
+++
@@ -1,7 +0,0 @@
-function r = hdlcoder_board_customization
-%HDLCODER_BOARD_CUSTOMIZATION Register custom HDL Workflow Advisor boards.
-
-    r = { ...
-        'Ultra96V2.plugin_board' ...
-    };
-end
